4257f007b2c70b2d98a708b21d5ba9d88e22bdbc
Edward <edward.jahoda@gmail.com>
2021-01-28 10:46:47 -0500

    create file mapping mis:ins -> Dataset names

---
+++ run_once/couples_to_dataset.py
@@ -0,0 +1,58 @@
+# create a dict of (mis/ins): dataset_file names
+import json
+import glob
+from collections import defaultdict
+
+
+def standardize_and_tag(mission, instrument, aliases):
+    if mission in aliases["mission_aliases"]:
+        mission = aliases["mission_aliases"][mission].lower()
+
+    if instrument in aliases["instrument_aliases"]:
+        instrument = aliases["instrument_aliases"][instrument]
+
+    return mission if instrument == 'not applicable' else mission + ":" + instrument
+
+
+def remove_duplicate_list_entries(input_list):
+    # remove the duplicate entry (preserve order)
+    seen = set()
+    unique = []
+    for item in input_list:
+        if item not in seen:
+            unique.append(item)
+        seen.add(item)
+
+    return unique
+
+
+dataset_directory = 'C:/Users/edwar/Desktop/Publishing Internship/datasets'
+output_file_name = 'couples_to_datasets.json'
+output_file_location = '../data/json/' + output_file_name
+
+data = defaultdict(list)
+
+with open('../data/json/aliases.json', encoding='utf-8') as f:
+    aliases = json.load(f)
+
+for file in glob.glob(dataset_directory + "/*.json"):
+    print(file)
+    with open(file, errors='ignore') as f:
+        contents = json.load(f)
+
+    dataset_name = contents['CollectionCitations'][0]['SeriesName']
+    platforms = contents['Platforms']
+    for index, elements in enumerate(platforms):  # will sometimes be a list
+        short_mission = elements.get('ShortName', '').lower()
+
+        instruments = elements['Instruments']
+        for instrument in instruments:
+            short_instrument = instrument.get('ShortName', '').lower()
+            tag = standardize_and_tag(short_mission, short_instrument, aliases)
+            data[tag].append(dataset_name)
+
+for key, value in data.items():
+    data[key] = remove_duplicate_list_entries(value)
+
+with open(output_file_location, 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=4)
