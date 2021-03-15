27bbc815283bd30246642f096caec6998776ed93
Edward <edward.jahoda@gmail.com>
2021-03-15 12:04:41 -0400

    correct missed extraneous counts/stats for both restricted and non-restricted versions

--- CMR_queries/export_to_csv.py
+++ CMR_queries/export_to_csv.py
@@ -5,7 +5,9 @@
 
 import json
 import re
+from collections import defaultdict
 
+filename = 'Aura_mls_cme'
 with open('18-55-27features.json', encoding='utf-8') as f:
     features = json.load(f)
 
@@ -29,7 +31,15 @@ def format_lot(lot):
     return lot_str
 
 
-def dump_data(key, features, csv, manually_reviewed=None, title=''):
+def correct_missed_extraneous(ground_truths, predictions):
+
+    ground_truths = set(ground_truths)
+    correct = predictions & ground_truths
+    missed = ground_truths - predictions
+    extraneous = predictions - ground_truths
+    return correct, missed, extraneous
+
+def dump_data(key, features, csv, manually_reviewed=None, title='', running_cme_stats=None):
 
     summary_stats = features['summary_stats']
     couples = sorted(list(summary_stats['valid_couples'].items()), key=lambda x: x[1], reverse=True)
@@ -51,17 +61,39 @@ def dump_data(key, features, csv, manually_reviewed=None, title=''):
     cmr_list = ';'.join(list(cmr_results))
     csv += f',{cmr_list}'
 
+    if manually_reviewed:
+        correct, missed, extraneous = correct_missed_extraneous(manually_reviewed['manually_reviewed'], cmr_results)
+        running_cme_stats['correct_count'] += len(correct)
+        running_cme_stats['missed_count'] += len(missed)
+        running_cme_stats['extraneous_count'] += len(extraneous)
+        for corr in correct:
+            running_cme_stats['correct_dict'][corr] += 1
+        for miss in missed:
+            running_cme_stats['missed_dict'][miss] += 1
+        for extra in extraneous:
+            running_cme_stats['extraneous_dict'][extra] += 1
+        csv += f',,,{len(correct)}, {len(missed)}, {len(extraneous)}'
     return csv + "\n"
 
-added_pdfs = set()
 
-csv = "paper, title, mission/instruments, models, manually reviewed, CMR datasets\n"
+
+
+added_pdfs = set()
+running_cme_stats = {
+    "correct_count": 0,
+    "missed_count": 0,
+    "extraneous_count": 0,
+    "correct_dict": defaultdict(int),
+    "missed_dict": defaultdict(int),
+    "extraneous_dict": defaultdict(int)
+}
+csv = "paper, title, mission/instruments, models, manually reviewed, CMR datasets,,,correct, missed, extraneous\n"
 # iterate through the manually reviewed ones. Insert it into the paper applicable if possible
 for parent_key, value in key_title_ground_truth.items():
     pdf_key = value['pdf']
     added_pdfs.add(pdf_key)
     if pdf_key in features:
-        csv = dump_data(pdf_key, features[pdf_key], csv, manually_reviewed=value, title=value['title'])
+        csv = dump_data(pdf_key, features[pdf_key], csv, manually_reviewed=value, title=value['title'], running_cme_stats=running_cme_stats)
 
 for key, value in features.items():
     if key not in added_pdfs:
@@ -78,6 +110,12 @@ for key, value in features.items():
 #     csv += f'{key},{format_lot(couples)}, {format_lot(models)}\n'
 
 
+running_cme_stats['correct_dict'] = dict(sorted(running_cme_stats['correct_dict'].items(), key=lambda x: x[1], reverse=True))
+running_cme_stats['missed_dict'] = dict(sorted(running_cme_stats['missed_dict'].items(), key=lambda x: x[1], reverse=True))
+running_cme_stats['extraneous_dict'] = dict(sorted(running_cme_stats['extraneous_dict'].items(), key=lambda x: x[1], reverse=True))
 
-with open('Aura_mls.csv', 'w', encoding='utf-8') as f:
+with open(filename + '.json', 'w', encoding='utf-8') as f:
+    json.dump(running_cme_stats, f, indent=4)
+
+with open(filename + '.csv', 'w', encoding='utf-8') as f:
     f.write(csv)
