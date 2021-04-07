d3154e34ae9ddd7713c1f4a3df129c00348b0fd9
Edward <edward.jahoda@gmail.com>
2021-04-07 12:56:09 -0400

    modified section extraction to perform better with edge cases

--- convert_using_cermzones/extract_relevant_sections.py
+++ convert_using_cermzones/extract_relevant_sections.py
@@ -12,10 +12,10 @@ import re
 # text_location = 'data/cermine_results/cermzones_text/'
 # output_location = 'data/cermine_results/cermzones_preprocess/'
 
-text_location = 'text/'
-output_location = 'preprocessed/'
+text_location = 'aura-mls/text/'
+output_location = 'aura-mls/preprocessed/'
 
-paragraph_labels = ["Abstract", "Data", "MLS", "Aura", "OMI", "Methods", "Instruments", "Measurements", "Acknowledgements"]
+paragraph_labels = ["Data", "MLS", "Aura", "OMI", "Methods", "Instruments", "Measurements", "Acknowledgements"]
 sections_to_avoid = ['introduction', 'references']
 
 
@@ -28,6 +28,11 @@ for file_name in tqdm(glob.glob(text_location + '*.txt')):
 
         text = f.read()
 
+        if file_name != "aura-mls/text\\548TE5LI.txt":
+            continue
+
+        print(file_name)
+
         paragraphs = text.split('\n\n')
         # print(paragraphs)
         for para in paragraphs:
@@ -38,10 +43,10 @@ for file_name in tqdm(glob.glob(text_location + '*.txt')):
                 # if label.lower() in pg_title.lower():
                 #     print("HERE")
 
+            pg_title = re.sub(r'[0-9]+\.? ?', '', pg_title).strip().lower()  # remove numbers, '.', and space. So 1. Introduction -> introduction
+
             important_section_name = any(label.lower() in pg_title.lower() for label in paragraph_labels)
             important_words_in_text = any(label.lower() in pg_text_a.lower() for label in paragraph_labels for pg_text_a in pg_text)
-
-            pg_title = re.sub(r'[0-9]+', '', pg_title).strip()
 
             if important_section_name:
                 relevant_sections += " ".join(pg_text) + "\n"
