67c3c0c57579d2081a0386eec08b1f72c8b39a52
Edward <edward.jahoda@gmail.com>
2021-04-14 17:09:26 -0400

    improving doi/long name searching

--- explicit_citation_label/exp.py
+++ explicit_citation_label/exp.py
@@ -4,6 +4,6 @@ text = 'https://disc.gsfc.nasa.gov/'
 file_name = 'temp'
 keyword = r'disc.gsfc.nasa.gov'
 
-occurrences = re.findall(r'.{,70}' + keyword + '.{,70}', text)
+occurrences = re.findall(r'.{,70}\n?.{,70}' + keyword + '.{,70}\n?.{,70}', text)
 if occurrences:
     print(occurrences, file_name)
--- explicit_citation_label/explicit_references.py
+++ explicit_citation_label/explicit_references.py
@@ -3,71 +3,95 @@ import re
 import glob
 import json
 
-
-# {'GEN_REFERENCES', 'MET_CORRESPONDENCE', 'MET_ABSTRACT', 'GEN_OTHER' (like pg number), 'MET_DATES', 'MET_AFFILIATION', 'BODY_HEADING', 'BODY_CONTENT', 'MET_BIB_INFO', 'MET_TITLE', 'MET_AUTHOR'}
-reference_label = "GEN_REFERENCES"
-cermzones_directory = '../convert_using_cermzones/giovanni/successful_cermfiles/'
-
-results = {}
-
 '''
-    Look for , http://disc.sci.gsfc.nasa.
-gov/Aura/data-holdings/OMI/omso2_v003.shtml. ?
-Or Foddard Earth Sciences
+1. Split citations. end in '.\n' and lookahead for Last, F.
+
+NER model to pick up people/organizations? Probably overkill
+
+    Look for http://disc.sci.gsfc.nasa Or Goddard Earth Sciences in addition to explicit mentions
 '''
 
-with open('../data/json/doi_to_dataset_name.json') as f:
-    doi_to_dataset = json.load(f)
 
-dataset_to_doi = {v: k for k, v in doi_to_dataset.items()}
-
-with open('../data/json/dataset_long_to_short.json') as f:
-    dataset_long_to_short = json.load(f)
-
-keyword = r'disc\.gsfc\.nasa\.gov'
-
-papers_count = 0
-for file in glob.glob(cermzones_directory + "*.cermzones"):
-    dois_founds = []
-    # print(file)
-    file_name = file.split("\\")[-1]
-
-    tree = ET.parse(file)
-    root = tree.getroot()
-
-    text = ""
-    # Bullet symbol has ordinal 8226
-    for child in root.findall('./zone'):
-        # print(child.tag, child.attrib)
-        if child.attrib['label'] == reference_label:
-            # print(child.text)
-            text = child.text
-
-            occurrences = re.findall(r'.{,70}' + keyword + '.{,70}', text)
-            if occurrences:
-                print(occurrences, file_name)
+def search_occurrences(keyword):
+    occurrences = re.findall(r'.{,130}\n?.{,130}' + keyword + '.{,70}\n?.{,70}', text)
+    if occurrences:
+        print(occurrences, file_name)
 
 
-            # for doi in doi_to_dataset:
-            #     matches = re.findall(rf'{doi}', text)
-            #     # print(matches)
-            #     if len(matches) >= 1:
-            #         dois_founds.append(doi)
-            #
-            # for long_name, short_name in dataset_long_to_short.items():
-            #     matches = re.findall(rf'{long_name}', text) + re.findall(rf'{short_name}', text)
-            #     if len(matches) >= 1:
-            #         dois_founds.append(short_name)
+if __name__ == '__main__':
+    # User parameters
+    output_file_name = "giovanni_exp_2.json"
+    cermzones_directory = '../convert_using_cermzones/giovanni/successful_cermfiles/'
+    doi_to_dataset_mapping_location = '../data/json/doi_to_dataset_name.json'
+    dataset_long_to_short_mapping = '../data/json/dataset_long_to_short.json'
 
-    if len(dois_founds) >= 1:
-        print(file_name, dois_founds)
-        papers_count += 1
-        modified_file_name = file_name.split('.')[0]
-        results[modified_file_name] = dois_founds
 
-# print("papers count", papers_count)
-# with open('Giovanni_explicit.json', 'w', encoding='utf-8') as f:
-#     json.dump(results, f, indent=4)
+    reference_label = "GEN_REFERENCES"
+    keyword = r'disc\.gsfc\.nasa\.gov'
+    papers_with_explicit_mentions = 0
+    results = {}
+
+    with open(doi_to_dataset_mapping_location) as f:
+        doi_to_dataset = json.load(f)
+
+    dataset_to_doi = {v: k for k, v in doi_to_dataset.items()}
+
+    with open(dataset_long_to_short_mapping) as f:
+        dataset_long_to_short = json.load(f)
+
+    for file in glob.glob(cermzones_directory + "*.cermzones"):
+        dois_founds = []
+        datasets_found = []
+        # print()
+        # print()
+        # print(file)
+        file_name = file.split("\\")[-1]
+
+        tree = ET.parse(file)
+        root = tree.getroot()
+
+        for child in root.findall('./zone'):
+            if child.attrib['label'] == reference_label:
+                text = child.text
+                print(text)
+                print("--------")
+                # attempt to split between references
+                splits = re.split(r'\.\n(?=(\d+\.? )?\w+, \w)', text)  # ends citation with '.' Next line starts with LastName, First inital and ignore starting numbers ie: 7.
+                # is not perfect. Things like 8. World Meteorological Organization (WMO). S
+                for s in splits:
+                    print(s, '\n')
+                print("--------")
+
+
+                # search_occurrences()
+
+                for doi in doi_to_dataset:
+                    matches = re.findall(rf'{doi}', text)
+                    # print(matches)
+                    if len(matches) >= 1:
+                        dois_founds.append(doi)
+                        dataset_found = doi_to_dataset[doi]
+                        datasets_found.append(dataset_found)
+
+                for long_name, short_name in dataset_long_to_short.items():
+                    long_matches = re.findall(rf'{long_name}', text)
+                    short_matches = re.findall(rf'{short_name}', text)
+                    if len(long_matches) >= 1:
+                        dataset_found = [dataset_long_to_short[lm] for lm in long_matches]
+                        datasets_found += dataset_found
+                    elif len(short_matches) >= 1:
+                        datasets_found += short_matches
+
+
+        if len(dois_founds) >= 1 or len(datasets_found) >= 1:
+            print(file_name, dois_founds, datasets_found)
+            papers_with_explicit_mentions += 1
+            modified_file_name = file_name.split('.')[0]
+            results[modified_file_name] = dois_founds
+
+    print("papers with explicit dataset citations", papers_with_explicit_mentions)
+    with open(output_file_name, 'w', encoding='utf-8') as f:
+        json.dump(results, f, indent=4)
 
 
 
