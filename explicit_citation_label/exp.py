af699c6e11565d45e1c4154ce65db0b0ef19fff2
Edward <edward.jahoda@gmail.com>
2021-04-19 11:11:16 -0400

    search GES DISC for everything except GIOVANNI

--- explicit_citation_label/exp.py
+++ explicit_citation_label/exp.py
@@ -1,9 +1,33 @@
 import re
 
-text = 'https://disc.gsfc.nasa.gov/'
-file_name = 'temp'
-keyword = r'disc.gsfc.nasa.gov'
+# text = 'https://disc.gsfc.nasa.gov/'
+# file_name = 'temp'
+# keyword = r'disc.gsfc.nasa.gov'
+#
+# occurrences = re.findall(r'.{,70}\n?.{,70}' + keyword + '.{,70}\n?.{,70}', text)
+# if occurrences:
+#     print(occurrences, file_name)
 
-occurrences = re.findall(r'.{,70}\n?.{,70}' + keyword + '.{,70}\n?.{,70}', text)
-if occurrences:
-    print(occurrences, file_name)
+text = '''
+9. Hu man, G.J.; Adler, R.F.; Bolvin, D.T.; Nelkin, E.J. The TRMM Multi-satellite Precipitation Analysis (TMPA).
+Chapter 1. In Satellite Rainfall Applications for Surface Hydrology; Springer: Dordrecht, The Netherlands, 2010;
+pp. 3-22. [CrossRef]
+10. Horishima, K. Rainfall observation from Tropical Rainfall Measuring Mission (TRMM) satellite. J. Vis. 1999,
+2, 93-98.
+'''
+temp = re.split(r'\.( \[CrossRef\])', text)
+print(temp)
+splits = re.split(r'\. \[CrossRef\]\n(?=(\d{1,2}\.? )?\w+, \w)', text)  # ends citation with '.' Next line starts with LastName, First inital and ignore starting numbers ie: 7.
+                # is not perfect.
+
+for s in splits:
+    print(s, '\n')
+
+sent = 'I ate some food'
+print(re.split(r'a(te)?', sent))
+
+keyword = r'(?:disc\.gsfc\.nasa\.gov)|(?:GES[ -]?DISC)'
+s = '''18. Amy McNally NASA; GSFC; HSL. FLDAS Noah Land Surface Model L4 Global Monthly 0.1 0.1 Degree (MERRA-2 and CHIRPS);
+Goddard Earth Sciences Data and Information Services Center (GESDISC): Greenbelt, MD, USA, 2018. [CrossRef]'''
+
+print(re.findall(keyword, s))
--- explicit_citation_label/explicit_references.py
+++ explicit_citation_label/explicit_references.py
@@ -20,16 +20,18 @@ def search_occurrences(keyword):
 
 if __name__ == '__main__':
     # User parameters
-    output_file_name = "giovanni_exp_2.json"
-    cermzones_directory = '../convert_using_cermzones/giovanni/successful_cermfiles/'
+    output_file_name = "aura_mls_doi_dataset_map_gd_link_false.json"
+    cermzones_directory = '../convert_using_cermzones/aura-mls/successful_cermfiles/'
     doi_to_dataset_mapping_location = '../data/json/doi_to_dataset_name.json'
     dataset_long_to_short_mapping = '../data/json/dataset_long_to_short.json'
+    search_text_too = False
 
 
     reference_label = "GEN_REFERENCES"
-    keyword = r'disc\.gsfc\.nasa\.gov'
+    keyword = r'(?:disc\.gsfc\.nasa\.gov)|(?:GES[ -]?DISC)'
     papers_with_explicit_mentions = 0
     results = {}
+    free_text = True
 
     with open(doi_to_dataset_mapping_location) as f:
         doi_to_dataset = json.load(f)
@@ -40,54 +42,109 @@ if __name__ == '__main__':
         dataset_long_to_short = json.load(f)
 
     for file in glob.glob(cermzones_directory + "*.cermzones"):
-        dois_founds = []
-        datasets_found = []
+        dois_founds = set()
+        datasets_found = set()
+        datasets_found_map = set()
+        free_text_ges_disc_citations = []
         # print()
         # print()
-        # print(file)
-        file_name = file.split("\\")[-1]
+        print(file)
+        file_name = file.split("\\")[-1].replace(".cermzones", "")
 
         tree = ET.parse(file)
         root = tree.getroot()
 
+
         for child in root.findall('./zone'):
-            if child.attrib['label'] == reference_label:
+            if child.attrib['label'] == reference_label or search_text_too:
                 text = child.text
-                print(text)
-                print("--------")
-                # attempt to split between references
-                splits = re.split(r'\.\n(?=(\d+\.? )?\w+, \w)', text)  # ends citation with '.' Next line starts with LastName, First inital and ignore starting numbers ie: 7.
-                # is not perfect. Things like 8. World Meteorological Organization (WMO). S
-                for s in splits:
-                    print(s, '\n')
-                print("--------")
+                # print(text)
+                # print("--------")
+                # Remove words that might mess up splitting
+                text = re.sub(r' \[CrossRef\] ?', '', text)
+
+                if not free_text:
+                    for doi in doi_to_dataset:
+                        matches = re.findall(rf'{doi}', text)
+                        # print(matches)
+                        if len(matches) >= 1:
+                            dois_founds.add(doi)
+                            dataset_found = doi_to_dataset[doi]
+                            datasets_found_map.add(dataset_found)
+                            found = True
+
+                    for long_name, short_name in dataset_long_to_short.items():
+                        long_matches = re.findall(rf'{long_name}', text)
+                        short_matches = re.findall(rf'{short_name}', text)
+                        if len(long_matches) >= 1:
+                            dataset_found = [dataset_long_to_short[lm] for lm in long_matches]
+                            # datasets_found += dataset_found
+                            [datasets_found.add(ds) for ds in dataset_found]
+                            [datasets_found_map.add(ds) for ds in dataset_found]
+                            found = True
+                        elif len(short_matches) >= 1:
+                            # datasets_found += short_matches
+                            found = True
+                            [datasets_found.add(ds) for ds in short_matches]
+                            [datasets_found_map.add(ds) for ds in short_matches]
+                else:
+                    # attempt to split between references
+                    splits = re.split(r'\.\n(?=(\d{1,2}\.? )?\w+, \w)', text)  # ends citation with '.' Next line starts with LastName, First inital and ignore starting numbers ie: 7.
+                    # is not perfect. Things like 8. World Meteorological Organization (WMO). S
+                    for s in splits:
+                        if not s:
+                            continue
+                        # print(s, '\n')
+                        found = False
+                        for doi in doi_to_dataset:
+                            matches = re.findall(rf'{doi}', s)
+                            # print(matches)
+                            if len(matches) >= 1:
+                                dois_founds.add(doi)
+                                dataset_found = doi_to_dataset[doi]
+                                datasets_found_map.add(dataset_found)
+                                found = True
+
+                        for long_name, short_name in dataset_long_to_short.items():
+                            long_matches = re.findall(rf'{long_name}', s)
+                            short_matches = re.findall(rf'{short_name}', s)
+                            if len(long_matches) >= 1:
+                                dataset_found = [dataset_long_to_short[lm] for lm in long_matches]
+                                # datasets_found += dataset_found
+                                [datasets_found_map.add(ds) for ds in dataset_found]
+                                found = True
+                            elif len(short_matches) >= 1:
+                                # datasets_found += short_matches
+                                found = True
+                                [datasets_found_map.add(ds) for ds in short_matches]
+
+                        if not found:
+                            # keyword_occurrences = re.findall(r'.*\n?.*\n?.{,130}' + keyword + '.{,70}\n?.{,70}', s)
+                            keyword_occurrences = re.findall(rf'{keyword}', s)
+                            if len(keyword_occurrences) >= 1:
+                                print(file_name, s, '-----\n')
+                                free_text_ges_disc_citations.append(s)
 
 
-                # search_occurrences()
 
-                for doi in doi_to_dataset:
-                    matches = re.findall(rf'{doi}', text)
-                    # print(matches)
-                    if len(matches) >= 1:
-                        dois_founds.append(doi)
-                        dataset_found = doi_to_dataset[doi]
-                        datasets_found.append(dataset_found)
-
-                for long_name, short_name in dataset_long_to_short.items():
-                    long_matches = re.findall(rf'{long_name}', text)
-                    short_matches = re.findall(rf'{short_name}', text)
-                    if len(long_matches) >= 1:
-                        dataset_found = [dataset_long_to_short[lm] for lm in long_matches]
-                        datasets_found += dataset_found
-                    elif len(short_matches) >= 1:
-                        datasets_found += short_matches
+                # print("--------")
 
 
-        if len(dois_founds) >= 1 or len(datasets_found) >= 1:
-            print(file_name, dois_founds, datasets_found)
+
+
+
+        if len(dois_founds) >= 1 or len(datasets_found_map) >= 1 or len(free_text_ges_disc_citations):
+            print(file_name, dois_founds, datasets_found_map)
             papers_with_explicit_mentions += 1
             modified_file_name = file_name.split('.')[0]
-            results[modified_file_name] = dois_founds
+            results[modified_file_name] = {
+                "explicit_dois": list(dois_founds),
+                "explicit_datasets": list(datasets_found),
+                "datasets_and_doi": list(datasets_found_map),
+                "free_text": free_text_ges_disc_citations
+            }
+
+
 
     print("papers with explicit dataset citations", papers_with_explicit_mentions)
     with open(output_file_name, 'w', encoding='utf-8') as f:
